63f320f5c989a02ab542f63f509466480f921f8b
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-02-01 13:54:25 +0100

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_architecture_starter/src/app.dart';
+import 'package:flutter_clean_architecture_starter/src/app/helpers/my_cubit_observer.dart';
 import "./injection_container.dart" as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  Bloc.observer = MyBlocObserver();
   await di.init();
   runApp(MultiBlocProvider(providers: [
     // bloc providers
